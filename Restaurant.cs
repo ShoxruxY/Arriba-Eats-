26953f679f3f3d0c899552efc329a0a1fd4556b3
Yusupov Shokhrukhmirzo <yusupovshoxruxmirzo.mit@gmail.com>
2025-08-08 11:25:23 +1000

    The first files

---
+++ Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Restaurant
+    {
+        public static List<string> restaurantNames = new List<string>();
+    }
+    internal class Order
+    {
+        
+        public string orderStatus;
+        public string customerEmailAddress;// method to get total orders and amount
+
+
+    }
+}
